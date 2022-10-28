3cdc69782c346cb5f9a3fff319209746e63b35cd
unknown <coln.porter@gmail.com>
2022-10-28 16:38:54 -0700

    started cookie and login functionality

--- routes/account-routes.js
+++ routes/account-routes.js
@@ -2,19 +2,41 @@ const express = require('express');
 const router  = express.Router();
 const DB = require('../db-calls/db-requests.js')
 
-
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 
 // maps from '/accounts/...'
 
+function tokenGenerator() {
+    return crypto.randomBytes(20).toString('base64').slice(0 , 19)
+}
+
 router
-    .get('/signin' , async (req , res) => {
-        res.send('<div>hi</div>')
+    .post('/signin' , async (req , res) => {
+        DB.getAccount(req.body.email)
+            .then(async result => {
+                if(await bcrypt.compare(req.body.password, result[0]['password'])) {
+
+                    const newToken = tokenGenerator()
+
+                    res.cookie('session-cookie', newToken , {
+                        secure: true,
+                        httpOnly: true,
+                    })
+
+                    DB.createSession(req.body.email, 5, newToken) 
+                        .then(async () => res.sendStatus(200))
+                        .catch(error => { console.log(error); res.sendStatus(500)})
+                } // password do not match
+                else {res.sendStatus(401)}
+            })
+            .catch(error => {res.send(error)})
     })
     .post('/createaccount', async (req , res) => {
-
-        DB.createAccount(req.body.username , req.body.password , req.body.email , req.body.role)
+        const hashedPassword = await bcrypt.hash(req.body.password , 10)
+        DB.createAccount(req.body.username , hashedPassword , req.body.email , req.body.role)
             .then(()  => res.sendStatus(200))
-            .catch(error => {res.sendStatus(500)})
+            .catch(error => {console.log("ERROR" , error) ; res.sendStatus(500)})
     })
 
 module.exports = router;
